7c72798c1f45f6e1a0d86c062267e7b896bf6508
Aura <Aura@LAPTOP-EV4E4LU5>
2017-12-01 12:22:09 +0100

    new class

---
+++ userService/src/main/java/iou/api/JSONMapper.java
@@ -0,0 +1,18 @@
+package iou.api;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JSONMapper<Type>
+{
+	public String objectToJSON(Type object) throws IOException
+	{
+		return new ObjectMapper().writeValueAsString(object);
+	}
+	
+	public Type jsonToObject(String json, Class<Type> typeParameterClass) throws IOException
+	{
+		return new ObjectMapper().readValue(json, typeParameterClass);
+	}
+}
