4b4663f3da19e56259b10220b75300b25c6e68b9
Jonas Degenhard <degenhard@white-paper-media.de>
2017-12-05 10:28:44 +0100

    ???

--- userService/src/main/java/iou/resources/FriendsResource.java
+++ userService/src/main/java/iou/resources/FriendsResource.java
@@ -6,10 +6,9 @@ import iou.api.UserService;
 import iou.models.Person;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.Set;
 
 @Path("/friends")
@@ -30,4 +29,15 @@ public class FriendsResource {
     {
         return userService.getFriends(2);
     }
+
+    @POST
+    @UnitOfWork
+    @Path("/create")
+    public Response create(@PathParam("name") String name)
+    {
+        userService.createFriend(2, name, "Doe");
+        return Response.ok().build();
+
+    }
+
 }
